c73a7f6db4d853efb5b381d53c986900d1b15c24
ninel.hodzic <ninel.hodzic@gmail.com>
2026-05-22 12:20:22 +0200

    chore(packages): generate declarations with tsc across packages

--- packages/connectors/tsup.config.ts
+++ packages/connectors/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
-  dts: { compilerOptions: { composite: false } },
+  dts: false,
   clean: true,
   target: 'node20',
   sourcemap: true,
