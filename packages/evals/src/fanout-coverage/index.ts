90db8f9b115447046683d4dbd5c70274547dfa42
ninel.hodzic <ninel.hodzic@gmail.com>
2026-07-05 17:32:44 +0200

    feat(evals): add deterministic fanout coverage scoring

---
+++ packages/evals/src/fanout-coverage/index.ts
@@ -0,0 +1,11 @@
+export {
+  runDeterministicFanoutCoverageEval,
+  scoreFanoutCoverageReport,
+} from "./fanout-coverage.js";
+export type {
+  FanoutCoverageDispatch,
+  FanoutCoverageEvalInput,
+  FanoutCoverageEvalReport,
+  FanoutCoverageScore,
+  FanoutCoverageStatus,
+} from "./fanout-coverage.js";
